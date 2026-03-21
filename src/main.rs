54d3e4853fee2f761cc056852075ad2adc7f4f58
SantiagoLifischtz <santilifischtz@gmail.com>
2026-03-21 02:05:35 -0300

    faster pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::process::{Command, Stdio, Output};
-use std::{env, io};
+use std::process::{ChildStdout, Command, Output, Stdio};
+use std::{env, io, vec};
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 
 type Pixel = Vec<u8>;
 type Frame = Vec<Pixel>;
-type PackedFrame = Vec<u64>;
+type FrameBitmap = Vec<u64>;
 
 fn ffmpeg_command(input: &String) -> Output {
     let output = Command::new("ffmpeg")
@@ -17,6 +17,16 @@ fn ffmpeg_command(input: &String) -> Output {
     output
 }
 
+fn ffmpeg_stream(input: &String) -> ChildStdout {
+    let mut child = Command::new("ffmpeg")
+    .args(["-i",input,"-f","rawvideo","-vf","scale=640:360","-fpsmax","10","-pix_fmt","rgb24","-"])
+    .stdout(Stdio::piped())
+    .spawn()
+    .unwrap();
+
+    child.stdout.take().unwrap()
+}
+
 fn group_vec<T: Clone>(v: &Vec<T>, n: usize) -> Vec<Vec<T>> {
     v.chunks(n).map(|chunk| chunk.to_vec()).collect()
 }
@@ -26,17 +36,14 @@ fn white_threshold(v: &Pixel) -> bool {
     luma > 128.0
 }
 
-fn pack_frame_bits(frame: &Frame) -> PackedFrame {
+fn pack_pixels(frame_pixels: &Frame) -> FrameBitmap {
     const CHUNK_SIZE: usize = 64;
-    let mut packed: PackedFrame = Vec::new();
+    let cluster_count: usize = (frame_pixels.len() + CHUNK_SIZE - 1) / CHUNK_SIZE;
 
-    for i in 0..frame.len() {
-        if i%CHUNK_SIZE == 0 {
-            packed.push(0);
-        }
+    let mut packed: FrameBitmap = vec![0u64; cluster_count];
 
-        let is_white = white_threshold(&frame[i]);
-        if is_white {
+    for i in 0..frame_pixels.len() {
+        if white_threshold(&frame_pixels[i]) {
             packed[i/CHUNK_SIZE] |= 1 << (i%CHUNK_SIZE);
         }
     }
@@ -44,7 +51,7 @@ fn pack_frame_bits(frame: &Frame) -> PackedFrame {
     packed
 }
 
-fn xor_packed_frames(f1: &PackedFrame, f2: &PackedFrame) -> PackedFrame {
+fn xor_bitmaps(f1: &FrameBitmap, f2: &FrameBitmap) -> FrameBitmap {
     let mut out = vec![0; f1.len()];
     for i in 0..out.len() {
         out[i] = f1[i] ^ f2[i];
@@ -53,7 +60,8 @@ fn xor_packed_frames(f1: &PackedFrame, f2: &PackedFrame) -> PackedFrame {
     out
 }
 
-fn get_frame_string(diff: PackedFrame) -> String {
+// TODO: return pure data instead of a string
+fn get_frame_bytes(diff: FrameBitmap) -> Vec<u8> {
     let mut out = String::new();
     for i in 0..diff.len() {
         for j in 0..64 {
@@ -67,10 +75,37 @@ fn get_frame_string(diff: PackedFrame) -> String {
     }
     out.push('\n');
 
-    out
+    out.into_bytes()
 }
 
-fn main() {
+fn get_frame_bitmap(rgb24_frame: &Vec<u8>) -> FrameBitmap {
+    let pixels = group_vec(rgb24_frame, 3);
+    
+    pack_pixels(&pixels)
+}
+
+fn run_pipeline(stream: &mut ChildStdout, frame_byte_size: usize, output_file: &mut File) {
+    let mut buffer = vec![0u8; frame_byte_size];
+
+    // Taking advantage of the starting all 0 buffer to generate an all 0 bitmap
+    let mut prev_frame = get_frame_bitmap(&buffer);
+
+    loop {
+        match stream.read_exact(&mut buffer) {
+            Ok(_) => {
+
+                let bitmap = get_frame_bitmap(&buffer);
+                let diff = xor_bitmaps(&prev_frame, &bitmap);
+                prev_frame = bitmap;
+
+                output_file.write_all(&get_frame_bytes(diff)).unwrap();
+            }
+            Err(_) => break
+        }
+    }
+}
+
+fn old_pipeline() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("Input file required");
@@ -100,12 +135,12 @@ fn main() {
 
     print!("Calculating frame deltas...");
     io::stdout().flush().unwrap();
-    let mut diff: Vec<PackedFrame> = Vec::new();
-    let mut prev_frame = pack_frame_bits(&frame_data[0]);
+    let mut diff: Vec<FrameBitmap> = Vec::new();
+    let mut prev_frame = pack_pixels(&frame_data[0]);
     
     for i in 1..frame_data.len() {
-        let frame = pack_frame_bits(&frame_data[i]);
-        diff.push(xor_packed_frames(&prev_frame, &frame));
+        let frame = pack_pixels(&frame_data[i]);
+        diff.push(xor_bitmaps(&prev_frame, &frame));
         prev_frame = frame;
     }
     println!(" Done");
@@ -115,8 +150,22 @@ fn main() {
     let mut file = File::create("output/output.2col").unwrap();
 
     for frame_delta in diff {
-        let line = get_frame_string(frame_delta);
-        file.write_all(line.as_bytes()).unwrap();
+        let line = get_frame_bytes(frame_delta);
+        file.write_all(&line).unwrap();
     }
     println!(" Done");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Input file required");
+    }
+    let input_filename = &args[1];
+
+    let mut output_file = File::create("output/output.2col").unwrap();
+    let mut stream = ffmpeg_stream(input_filename);
+    let frame_size = 640*360*3; // width * height * bytes_per_pixel
+
+    run_pipeline(&mut stream, frame_size, &mut output_file);
 }
